9548f5321f27f0ff3882cef0b530a6c64b30de2f
Savatneev Anton <savatneev-anton@mail.ru>
2023-01-02 03:31:22 +0300

    fix

--- src/Services/IceCaptchaService.php
+++ src/Services/IceCaptchaService.php
@@ -65,7 +65,7 @@ class IceCaptchaService
 
     public function getSecretKey(): string
     {
-        return Hashids::encodeHex(strval($this->captcha_text."&&".now()->timestamp));
+        return Hashids::encodeHex($this->captcha_text."&&".now()->timestamp);
     }
 
     public function generate(): static
